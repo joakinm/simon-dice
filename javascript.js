0d7368889d5df8a7526dcbd39e9522e935bfacd9
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-10 09:47:58 +0100

    mejorando codigo de simon

--- javascript.js
+++ javascript.js
@@ -1,57 +1,48 @@
-var i;
-var jugadas = 1;
-var activado;
-var arreglo = new Array();
+let i;
+let jugadas = 1;
+let activado;
+let arreglo = new Array();
 
-function nuevajugada() // muestra diferentes focos en alguno de los 4 botones
-{
-    activado = colorearobjeto();
-    setInterval(document.getElementById("boton"+ activado).focus(),3000);
+function nuevajugada() { // muestra diferentes focos en alguno de los 4 botones
+   activado = colorearobjeto();
+   setInterval(document.getElementById('boton'+ activado).focus(), 3000);
    arreglo[jugadas-1] = activado;
-   i=0;
-   //document.getElementById("comienzo").focus();
+   i = 0;
 }
 
-function repetiryagregar(){
-    var j;
-    for (j=0;j<=jugadas-1;j++)
-    {
-        setInterval(document.getElementById("boton"+ arreglo[j]).focus(),3000);
+async function repetiryagregar() {
+    let j;
+    for ( j = 0; j <= jugadas-1; j++ ) {
+        await ( setInterval(document.getElementById('boton'+ arreglo[j]).focus(), 3000));
     }
     jugadas++;
     nuevajugada();
 }
-function presiono(nro) //cuando el usuario aprieta un boton corre esta funcion, pasa el nro del boton
-{
-    if(arreglo.length != 0)
-    {
-        if(nro == arreglo[i])
-        {
+
+function presiono(nro) { //cuando el usuario aprieta un boton corre esta funcion, pasa el nro del boton
+    if (arreglo.length != 0) {
+        if (nro === arreglo[i]) {
             i++;
         }
-        else
-        {
-            alert("perdio!");
+        else {
+            alert('perdio!');
             arreglo = new Array();
-            jugadas=1;
+            jugadas = 1;
         }
-        if(i == jugadas)
-        {
-        alert("siguiente nivel");
+        if (i === jugadas) {
+        alert('siguiente nivel');
         repetiryagregar();
         }
     }
-    else
-    {
-        alert("no empezaste, o no empezaste de nuevo");
+    else {
+        alert('no empezaste, o no empezaste de nuevo');
     }
-    
 }
-function colorearobjeto()
-{
-    var num=Math.random();
-    if(num < 0.25) return 1;  // probabilidad a 1 
-    else if(num < 0.50) return 2; //  probabilidad a 2
-    else if(num < 0.75) return 3 // probabilidad a 3
+
+function colorearobjeto() {
+    const num = Math.random();
+    if ( num < 0.25 ) return 1;  // probabilidad a 1 
+    else if ( num < 0.50 ) return 2; //  probabilidad a 2
+    else if ( num < 0.75 ) return 3 // probabilidad a 3
     else return 4;  //probabilidad a 4
 }
